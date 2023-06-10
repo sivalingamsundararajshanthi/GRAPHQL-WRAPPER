419ac26f52a92ba81e08665d93f13024d13ce5f0
sivalingamsundararajshanthi <lingams200@gmail.com>
2023-06-09 23:04:52 -0400

    enable CORS for all requests

--- api/index.js
+++ api/index.js
@@ -2,8 +2,35 @@ import fetch from "node-fetch";
 import https from "https";
 import express from "express";
 import "dotenv/config";
+import cors from "cors";
 
 const app = express();
+
+// app.use(function (req, res, next) {
+//   // Website you wish to allow to connect
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+
+//   // Request methods you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+//   );
+
+//   // Request headers you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-Requested-With,content-type"
+//   );
+
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader("Access-Control-Allow-Credentials", true);
+
+//   // Pass to next layer of middleware
+//   next();
+// });
+
+app.use(cors());
 
 app.listen(3000, () => console.log("API server running in 3000"));
 
