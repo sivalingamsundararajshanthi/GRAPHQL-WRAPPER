2000d4c7a204170fe08c82f4a24de731903b6e73
sivalingamsundararajshanthi <lingams200@gmail.com>
2023-06-11 18:01:04 -0400

    remove security for app routes

--- api/index.js
+++ api/index.js
@@ -16,28 +16,28 @@ const app = express();
 
 app.use(cors()).listen(3000, () => console.log("API server running in 3000"));
 
-const authenticationRequired = async (req, res, next) => {
-  const authHeader = req.headers.authorization || "";
-  const match = authHeader.match(/Bearer (.+)/);
-  if (!match) {
-    return res.status(401).send();
-  }
+// const authenticationRequired = async (req, res, next) => {
+//   const authHeader = req.headers.authorization || "";
+//   const match = authHeader.match(/Bearer (.+)/);
+//   if (!match) {
+//     return res.status(401).send();
+//   }
 
-  try {
-    const accessToken = match[1];
-    if (!accessToken) {
-      return res.status(401, "Not authorized").send();
-    }
-    req.jwt = await oktaJwtVerifier.verifyAccessToken(accessToken, audience);
-    next();
-  } catch (err) {
-    return res.status(401).send(err.message);
-  }
-};
+//   try {
+//     const accessToken = match[1];
+//     if (!accessToken) {
+//       return res.status(401, "Not authorized").send();
+//     }
+//     req.jwt = await oktaJwtVerifier.verifyAccessToken(accessToken, audience);
+//     next();
+//   } catch (err) {
+//     return res.status(401).send(err.message);
+//   }
+// };
 
 // app.all("*", authenticationRequired); // Require authentication for all routes
 
-app.get("/getProducts", authenticationRequired, async function (req, res) {
+app.get("/getProducts", async function (req, res) {
   const query = `{
     getDemoProduct(id: 1230) {
       ModelName,
@@ -64,7 +64,7 @@ app.get("/getProducts", authenticationRequired, async function (req, res) {
   return res.json(response);
 });
 
-app.get("/getAllProducts", authenticationRequired, async function (req, res) {
+app.get("/getAllProducts", async function (req, res) {
   const query = `{
     getDemoProductListing {
       edges {
